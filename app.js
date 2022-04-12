546c0baeab3a4f67b30446e6788a8f889fa9f67f
Abdul-Sammad <100076834+Abdul-Sammad@users.noreply.github.com>
2022-04-13 02:45:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,148 @@
+
+// Chapter: 17-20
+
+// question.1_________________________________________*
+
+// var arr;
+// var arr = [
+//     [" ",  "  ",""],
+//     [" ",  "  ",""],
+//     [" ",  "  ",""]
+
+// ];
+
+
+// question.2_________________________________________*
+// var arr1 = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+
+
+// question.3_________________________________________*
+
+// for (var i = 1; i <=10; i++){
+//     document.write(i + "<br />");
+// }
+
+
+// question.4_________________________________________*
+
+// var tableNum = +prompt("Enter a number to show its multiplication table");
+// var tableLength = +prompt("Enter tablelength for table");
+
+// for ( var i = 1; i <= tableLength; i++){
+//     document.write(tableNum + "x" + i + " = " + i * tableNum + "<br />");
+// }
+
+
+// question.5_________________________________________*
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+//     for (var i = 0; i < fruits.length; i++){
+
+//         document.write(fruits[i] + "</br> </br>");
+        
+
+//  }
+
+//     var fruits = ["apple", "banana", "mango", "range","strawberry"];
+//     for (var i = 0; i < fruits.length; i++){
+//         document.write("Element of index " + i + " is " + fruits[i] + " </br>")
+    
+//   }
+
+    
+// question.6_________________________________________*
+
+// a) answer
+// document.write("Counting:");
+// for ( i = 1; i <= 15; i++) {
+        
+//     document.write(i+",");
+// }
+
+
+// b) answer
+
+// document.write("Reverse Counting:");
+// for ( i = 10; i >= 1; i--) {
+//     document.write(i+",");
+// }
+
+
+
+// c) answer
+
+// document.write("Even:");
+// for ( i = 0; i <= 20; i +=2) {
+//     document.write(i+",");
+// }
+
+// d) answer
+
+// document.write("odd:");
+// for ( i = 1; i < 20; i +=2) {        
+//     document.write(i+",");
+// }
+
+// d) answer
+
+// document.write("series:");
+// for ( i = 2; i <= 20; i +=2) {        
+//     document.write(i+"k,");
+// }
+
+
+
+// question.7_________________________________________*
+
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var useritem = prompt (" Welcom to ABC Bakery. What do yo want to order Sir/Ma'am?");
+// var matchFound = "no";
+// for (var i = 0; i < arr.length; i++){
+//     if (useritem === arr[i]){
+//         matchFound = "yes";
+//         alert( useritem +"is available at index" + arr[i] + "in our bakery");// i am unable to print index number in alert  }
+// }
+// if (matchFound === "no") {
+//     alert("we are sorry."+ useritem + "is not available in our bakery");
+// }  
+
+// question.8_________________________________________*
+// arr = [24, 53, 78, 91, 12];
+
+// var largerNum = arr[3];
+
+
+// for (var i = 1; i > arr.largerNum; i++){
+//     if(arr[i] > largerNum){
+//         largerNum = arr[i];
+//     }
+// }
+// alert("Larger Number is : "+ largerNum );
+
+
+// question.9_________________________________________*
+
+// arr = [24, 53, 78, 91, 12];
+// var smallest = arr[4];
+// for (var i = 1; i < arr.smallest; i++){
+//     if(arr[i] < smallest){
+//         smallest = arr[i];
+//     }
+// }
+// alert("Smallest Number is : "+ smallest );
+
+
+// question.10_________________________________________*
+
+// for ( var i = 0; i <= 100; i +=5){
+//     document.write(i+",");
+// }
+///   0 is also printing why
+
+
+
+
+
